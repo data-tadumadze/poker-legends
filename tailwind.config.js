ef9cdc664ecb5f0c4a0317e92566fc43d7174064
data-tadumadze <dtadu2018@agruni.edu.ge>
2023-04-24 18:46:51 +0400

    add media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,8 @@ module.exports = {
       xl: { max: "1160px" },
       // => @media (max-width: 1279px) { ... }
 
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
+      lg: { max: "1024px" },
+      // => @media (max-width: 1024px) { ... }
 
       md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
